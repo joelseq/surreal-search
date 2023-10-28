6cdaee1c9fed57a74b4e42efe5e8290c80bdf4c2
Joel Sequeira <joelseq96@gmail.com>
2023-10-28 03:08:52 -0400

    Add GH link and analytics

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,10 +1,22 @@
 <script>
 	import '../app.postcss';
 	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import Fa from 'svelte-fa';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
 </script>
 
 <AppBar>
 	<svelte:fragment slot="lead"><span /></svelte:fragment>
-	<svelte:fragment slot="trail"><LightSwitch rounded="rounded-full" /></svelte:fragment>
+	<svelte:fragment slot="trail">
+		<a
+			class="btn rounded-sm"
+			href="https://github.com/joelseq/surreal-search"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<Fa size="2x" icon={faGithub} class="mr-4" />
+		</a>
+		<LightSwitch rounded="rounded-full" />
+	</svelte:fragment>
 </AppBar>
 <slot />
